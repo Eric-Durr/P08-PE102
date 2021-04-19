03bb261c9360f216f5def0ebfcf5bed27d48325c
eric <eric.durr.20@ull.edu.es>
2021-04-19 11:31:13 +0100

    added documentation and tests passed

--- src/reduce.ts
+++ src/reduce.ts
@@ -1,4 +1,179 @@
+/**
+ * # Desarrollo de Array reduce mediante el patrón de diseño Template Method
+ * 
+ * Se propone desarrollar para esta actividad una versión de reduce mediante
+ * el patrón de diseño en TypeScript Template Method, el cual permite definir 
+ * un esqueleto algorítmico en una superclase que servirá de "plantilla" abstracta
+ * para el desarrollo de las funcionalidades en superclases.
+ * 
+ * En este caso la estructura que se propone es la siguiente:
+ * 
+ * Reduce (Clase abstracta / plantilla)
+ * |__> AddReduce
+ * |__> SubReduce
+ * |__> ProdReduce
+ * |__> DivReduce
+ * 
+ * Tal y cómo se puede apreciar, la superclase Reduce, que será abstracta será
+ * concretada por las clases hijas que implementan las 4 operaciones básicas esperadas
+ * 
+ */
 
-export function trueFunction(): boolean {
-  return true;
+
+export abstract class Reduce {
+  
+  protected valNum: number
+  
+  constructor(protected valuesArray: number[] , 
+              protected accNum: number = 0, )
+  {
+    this.valNum = 0;
+  }
+
+  /**
+   * ### operate template method
+   * 
+   * Este método define el esqueleto algorítmico que debe será empleado por las especificaciones, 
+   * en esencia es lo mismo para toda las clases derivadas, pero será ejecutado con las 
+   * especificaciones de los métodos particulares en cada clase
+   * 
+   * @returns accumulated value
+   */
+  public operate(): number {
+    this.valuesArray.forEach( (value)=>
+    {
+      this.copyValue(value);
+      this.computeAcc();
+    });
+    return this.returnACC();
+  }
+  
+  /**
+   * ### Copy value
+   * Operación implementada en la propia clase esqueleto. Copia el valor extraido
+   * en el atributo de valor actual.
+   * 
+   * @param currValue 
+   */
+  copyValue(currValue:number): void { this.valNum = currValue;}
+
+  /**
+   * ### Return Accumulator
+   * Operación implementada en la propia clase esqueleto. Devuelve el valor alojado 
+   * en el acumulador. al ser protected hay que otorgar algún tipo de seguridad de 
+   * acceso a esete valor al retornarlo.
+   * 
+   * @param currValue 
+   */
+  returnACC(): number { return this.accNum;}
+
+  /**
+   * ### Compute Accumulator
+   * Este método debe ser sobrecargado por las subclases.
+   * 
+   * Se encarga de calcular el valor del acumulador conforme a la especificación
+   * de cada una de las clases hija.
+   */
+  protected abstract computeAcc(): void;
+
+};
+
+/**
+ * # Clase de especificación de suma
+ * La Clase AddReduce concreta el método computeAcc(), que es donde
+ * se calcula el valor del acumulador, mediante una suma entre el valor
+ * previo del acumulador y el valor numerico copiado actualmente 
+ * 
+ * Esta toma por defecto en el acumulador el valor cero
+ */
+export class AddReduce extends Reduce {
+  
+  protected valNum: number
+  
+  constructor(protected valuesArray: number[] , 
+    protected accNum: number = 0)
+  { super(valuesArray, accNum)
+    this.valNum = 0;}
+
+  protected computeAcc(): void {
+    this.accNum = this.accNum + this.valNum;
+  }
 }
+
+
+/**
+ * # Clase de especificación de suma
+ * La Clase AddReduce concreta el método computeAcc(), que es donde
+ * se calcula el valor del acumulador, mediante una resta entre el valor
+ * previo del acumulador y el valor numerico copiado actualmente 
+ * 
+ * Esta toma por defecto en el acumulador el valor cero 
+ */
+export class SubReduce extends Reduce {
+  
+  protected valNum: number
+  
+  constructor(protected valuesArray: number[] , 
+    protected accNum: number = 0)
+  { super(valuesArray, accNum)
+    this.valNum = 0;}
+
+  protected computeAcc(): void {
+    this.accNum = this.accNum - this.valNum;
+  }
+}
+
+
+/**
+ * # Clase de especificación de multiplicación
+ * La Clase ProidReduce concreta el método computeAcc(), que es donde
+ * se calcula el valor del acumulador, mediante una multiplicación entre el valor
+ * previo del acumulador y el valor numerico copiado actualmente.
+ * 
+ * Esta toma por defecto en el acumulador el valor 1
+ *  
+ */
+export class ProdReduce extends Reduce {
+  
+  protected valNum: number
+  
+  constructor(protected valuesArray: number[] , 
+    protected accNum: number = 1)
+  { 
+    super(valuesArray, accNum)
+    this.valNum = 0;}
+
+  protected computeAcc(): void {
+    this.accNum = this.accNum * this.valNum;
+  }
+}
+
+
+/**
+ * # Clase de especificación de división
+ * La Clase DivReduce concreta el método computeAcc(), que es donde
+ * se calcula el valor del acumulador, mediante una división entre el valor
+ * previo del acumulador y el valor numerico copiado actualmente.
+ * 
+ * Esta toma por defecto en el acumulador el valor de la primera variable del array 
+ */
+export class DivReduce extends Reduce {
+  
+  protected valNum: number
+  
+  constructor(protected valuesArray: number[] , 
+    protected accNum: number = valuesArray[0])
+  { 
+    super(valuesArray, accNum)
+    this.valNum = 0;}
+
+  protected computeAcc(): void {
+    this.accNum = this.accNum / this.valNum;
+  }
+}
+
+
+
+
+
+
--- tests/reduce.spec.ts
+++ tests/reduce.spec.ts
@@ -1,10 +1,31 @@
 import 'mocha';
 import {expect} from 'chai';
-import {trueFunction} from '../src/reduce';
+import {AddReduce, SubReduce, ProdReduce, DivReduce} from '../src/reduce';
 
 
 describe("Testing array reduce method template implementation", ()=>{
-  it ("Empty test", ()=>{
-    expect(trueFunction()).to.be.true;
+  it ("AddReduce specification test - acc base 0", ()=>{
+    expect(new AddReduce([1,2,3,4]).operate()).to.be.eq(10);
+  });
+  it ("AddReduce specification test - acc base 5", ()=>{
+    expect(new AddReduce([1,2,3,4], 5).operate()).to.be.eq(15);
+  });
+  it ("SubReduce specification test - acc base 0", ()=>{
+    expect(new SubReduce([1,2,3,4]).operate()).to.be.eq(-10);
+  });
+  it ("SubReduce specification test - acc base 5", ()=>{
+    expect(new SubReduce([1,2,3,4], 5).operate()).to.be.eq(-5);
+  });
+  it ("ProdReduce specification test - acc base 0", ()=>{
+    expect(new ProdReduce([1,2,3,4], 0).operate()).to.be.eq(0);
+  });
+  it ("ProdReduce specification test ", ()=>{
+    expect(new ProdReduce([2,2,3,4]).operate()).to.be.eq(48);
+  });
+  it ("DivReduce specification test - acc base 0", ()=>{
+    expect(new DivReduce([2,2,3,4], 0).operate()).to.be.eq(0);
+  });
+  it ("DivReduce specification test ", ()=>{
+    expect(new DivReduce([10]).operate()).to.be.eq(1);
   });
 });
